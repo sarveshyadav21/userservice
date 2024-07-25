2fba6160d5e7a1c4f45e1fdb3db68ac1de68e264
sarveshyadav21 <97327998+sarveshyadav21@users.noreply.github.com>
2024-07-25 11:14:24 +0530

    Add files via upload

---
+++ src/test/java/com/google/userserivce/UserserivceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.google.userserivce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserserivceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
